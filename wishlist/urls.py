a12960b2cfeafbbe99a30efc9b9c76d2a3053b2c
ThomasMSmit <Thoom12@hotmail.com>
2021-08-16 19:29:33 +0000

    add functionality to wishlist app

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wishlist', views.wishlist, name='wishlist'),
+    path('', views.wishlist, name='wishlist'),
     path('add/<int:product_id>', views.add_to_wishlist, name='add_to_wishlist'),
     path('delete/<int:product_id>', views.delete_from_wishlist, name='delete_from_wishlist')
 ]
