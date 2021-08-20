0980e74693470961bd8160c68d9a958621053bd7
ThomasMSmit <Thoom12@hotmail.com>
2021-08-20 17:39:23 +0000

    add the review app

---
+++ reviews/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'review_title',
+        'product',
+    )
+
+    ordering = ('review_title',)
+
+
+admin.site.register(Review, ReviewAdmin)
